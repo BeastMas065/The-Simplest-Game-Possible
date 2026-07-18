caf83dad039fe2df36450c781b5e2ed961375ce9
Aqheel <aqheelsharief@gmail.com>
2026-07-18 20:09:05 +0530

    Basic Structure (Initial commit)

---
+++ Game/main/routes.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, render_template
+
+main = Blueprint('main', __name__)
+
+@main.route('/game')
+def Game():
+    page_title = "Game"
+    return render_template("main/Game.html", title=page_title)
+
+@main.route('/')
+def home():
+    page_title = "Simplest Game Possible"
+    return render_template("main/Home_Page.html", title=page_title)
+
+@main.route('/rules')
+def rules():
+    page_title = "Rules"
+    return render_template("main/rules.html", title=page_title)
+
+@main.route('/credits')
+def credits():
+    page_title = "Credits"
+    return render_template("main/credits.html", title=page_title)
+
+@main.route('/Review')
+def review():
+    page_title = "Review"
+    return render_template("main/review.html", title=page_title)
