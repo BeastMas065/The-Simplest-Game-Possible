a7a72b9ab20919954a86a51dae36c98c2063bae4
Aqheel <aqheelsharief@gmail.com>
2026-07-20 00:20:22 +0530

    added functionality on home page buttons

--- Game/static/script.js
+++ Game/static/script.js
@@ -5,7 +5,41 @@ const proceed_review1 = document.getElementById("proceed2review1");
 const proceed_review2 = document.getElementById("proceed2review2");
 const review_modal2 = document.getElementById("review2");
 const no_review2 = document.getElementById("no-review2");
+const mode_selector = document.getElementById("play-button");
+const mode_modal = document.getElementById("game-mode");
+const online = document.getElementById("online");
+const local = document.getElementById("local");
+const troll001 = document.getElementById("troll001");
+const play = document.getElementById("play");
+const rules = document.getElementById("rules");
+const credits = document.getElementById("credits");
 
+mode_selector.addEventListener("click", () => {
+    mode_modal.classList.add("open");
+});
+
+online.addEventListener("click", ()=> {
+    mode_modal.classList.remove("open");
+    troll001.classList.add("open");
+});
+
+local.addEventListener("click", ()=> {
+    mode_modal.classList.remove("open");
+    window.location.href = "/game";
+});
+
+play.addEventListener("click", () => {
+    troll001.classList.remove("open");
+    window.location.href = '/game';
+});
+
+rules.addEventListener("click", () => {
+    window.location.href = '/rules';
+});
+
+credits.addEventListener("click", () => {
+    window.location.href = '/credits';
+});
 
 review.addEventListener("click", () => {
     review_modal.classList.add("open");
