d3fc10d39386e268cbc54474eaae01a2074b8665
Aqheel <aqheelsharief@gmail.com>
2026-09-14 14:10:28 +0530

    main page done functionality and other pages remain

--- Game/main/routes.py
+++ Game/main/routes.py
@@ -3,6 +3,12 @@ from flask import Blueprint, render_template
 main = Blueprint('main', __name__)
 
 @main.route('/')
+@main.route('/Home')
 def home():
     page_title = "Simplest Game Possible"
     return render_template("main/Home_Page.html", title=page_title)
+
+@main.route("/Game")
+def game():
+    page_title = "Simplest Game Possible"
+    return render_template("main/game.html", title=page_title)
